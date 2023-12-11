3405e1f787c4598ce3bfb39fae72d5cc1ecd22d3
dianashyian88 <dvshyian@gmail.com>
2023-12-11 22:00:29 +0300

    update tasks

--- habits/services.py
+++ habits/services.py
@@ -7,6 +7,7 @@ class MyBot:
     TOKEN = settings.TELEGRAM_TOKEN
 
     def send_message(self, text, chat_id):
+        """Отправляет сообщения в ТГ"""
         requests.post(
             url=f'{self.URL}{self.TOKEN}/sendMessage',
             data={
@@ -14,3 +15,27 @@ class MyBot:
                 'text': text
             }
         )
+
+    def once_day_send_message(self, habit, day_delta, time_delta, now, text, chat_id):
+        """Отправляет сообщения в ТГ по привычкам с периодичностью раз в день"""
+        if day_delta / 60 > 60:
+            if 0 < time_delta < 1:
+                self.send_message(text, chat_id)
+                habit.last_reminder = now
+                habit.save()
+
+    def once_week_send_message(self, habit, day_delta, time_delta, now, text, chat_id):
+        """Отправляет сообщения в ТГ по привычкам с периодичностью раз в неделю"""
+        if 6 < day_delta / 3600 < 7:
+            if 0 < time_delta < 1:
+                self.send_message(text, chat_id)
+                habit.last_reminder = now
+                habit.save()
+
+    def twice_week_send_message(self, habit, day_delta, time_delta, now, text, chat_id):
+        """Отправляет сообщения в ТГ по привычкам с периодичностью 2 раза в неделю"""
+        if 1 < day_delta / 3600 < 2:
+            if 0 < time_delta < 1:
+                self.send_message(text, chat_id)
+                habit.last_reminder = now
+                habit.save()
--- habits/tasks.py
+++ habits/tasks.py
@@ -10,41 +10,30 @@ from django.utils import timezone
 def check_user_habit():
     list_user = User.objects.all()
     for obj in list_user:
-        if obj.telegram_id is not None:
-            now = datetime.datetime.now()
-            now = timezone.make_aware(now, timezone.get_current_timezone())
-            now_time = datetime.timedelta(hours=now.hour,
-                                          minutes=now.minute,
-                                          seconds=now.second)
-            list_habit = Habit.objects.filter(owner=obj.id,
-                                              is_enjoy_habit=False)
-            for habit in list_habit:
-                my_bot = MyBot()
-                text = f'Я буду {habit.action} в {habit.time} в {habit.place}'
-                chat_id = obj.telegram_id
-                habit_time = datetime.timedelta(hours=habit.time.hour,
-                                                minutes=habit.time.minute,
-                                                seconds=habit.time.second)
-                time_delta = (habit_time - now_time).total_seconds() / 3600
-                if habit.last_reminder is not None:
-                    day_delta = (now - habit.last_reminder).total_seconds()
-                else:
-                    day_delta = (now - habit.create_date).total_seconds()
-                if habit.periodicity == 'once_day':
-                    if day_delta / 60 > 60:
-                        if 0 < time_delta < 1:
-                            my_bot.send_message(text, chat_id)
-                            habit.last_reminder = now
-                            habit.save()
-                elif habit.periodicity == 'once_week':
-                    if 6 < day_delta / 3600 < 7:
-                        if 0 < time_delta < 1:
-                            my_bot.send_message(text, chat_id)
-                            habit.last_reminder = now
-                            habit.save()
-                elif habit.periodicity == 'twice_week':
-                    if 1 < day_delta / 3600 < 2:
-                        if 0 < time_delta < 1:
-                            my_bot.send_message(text, chat_id)
-                            habit.last_reminder = now
-                            habit.save()
+        if not obj.telegram_id:
+            return False
+        now = datetime.datetime.now()
+        now = timezone.make_aware(now, timezone.get_current_timezone())
+        now_time = datetime.timedelta(hours=now.hour,
+                                      minutes=now.minute,
+                                      seconds=now.second)
+        list_habit = Habit.objects.filter(owner=obj.id,
+                                          is_enjoy_habit=False)
+        for habit in list_habit:
+            my_bot = MyBot()
+            text = f'Я буду {habit.action} в {habit.time} в {habit.place}'
+            chat_id = obj.telegram_id
+            habit_time = datetime.timedelta(hours=habit.time.hour,
+                                            minutes=habit.time.minute,
+                                            seconds=habit.time.second)
+            time_delta = (habit_time - now_time).total_seconds() / 3600
+            if habit.last_reminder is not None:
+                day_delta = (now - habit.last_reminder).total_seconds()
+            else:
+                day_delta = (now - habit.create_date).total_seconds()
+            if habit.periodicity == 'once_day':
+                my_bot.once_day_send_message(habit, day_delta, time_delta, now, text, chat_id)
+            elif habit.periodicity == 'once_week':
+                my_bot.once_week_send_message(habit, day_delta, time_delta, now, text, chat_id)
+            elif habit.periodicity == 'twice_week':
+                my_bot.twice_week_send_message(habit, day_delta, time_delta, now, text, chat_id)
